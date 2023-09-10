418089c1e830e2f1d7f05e290ff05e429bee585b
Alex-G-R <alex.grabski555@gmail.com>
2023-09-10 18:50:53 +0200

    adding stuff

--- server.js
+++ server.js
@@ -6,5 +6,13 @@ const express = require('express');
 const server = express();
 const PORT = 3000;
 
+/* Show site */
+server.use(express.static('./public'))
+
+
+server.all('*', (req, res) => {
+    res.status(404).send('resource not found')
+})
+
 /* Setup server to be listening on the PORT */
 server.listen(PORT, ()=> console.log(`Server is listening on port ${PORT}...`))
