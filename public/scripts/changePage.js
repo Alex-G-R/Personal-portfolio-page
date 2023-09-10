833acb857d0e1c882a92a05c50a0fceec2abedc4
Alex-G-R <alex.grabski555@gmail.com>
2023-09-10 21:07:50 +0200

    final i think

--- public/scripts/changePage.js
+++ public/scripts/changePage.js
@@ -2,9 +2,18 @@ const contactButton = document.getElementById('contact-btn');
 const main = document.getElementById('main');
 const contactFormPage = document.getElementById('contact-form');
 const footer = document.getElementById('footer');
+const back = document.getElementById("back-btn")
 
 contactButton.addEventListener('click', () => {
     main.style.display = "none";
     contactFormPage.style.display = "flex";
     footer.style.marginTop = "930px"
-})
+    back.style.display= "block";
+});
+
+back.addEventListener('click', () => {
+    main.style.display = "block";
+    contactFormPage.style.display = "none";
+    footer.style.marginTop = "200px"
+    back.style.display= "none";
+});
